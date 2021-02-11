a6356263f064731d5b4012c8d8a45fbdd86f0cfa
Sara Nolen <nolensara1@gmail.com>
2021-02-11 21:16:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,11 @@
 class Transfer
-  # your code here
+
+  attr_accessor :status, :amount, :sender, :receiver
+
+  def initialize(amount, sender, receiver, status = "pending")
+    @amount = amount
+    @sender = sender
+    @receiver = receiver
+    @status = status
+  end
 end
