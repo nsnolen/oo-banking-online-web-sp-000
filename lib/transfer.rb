dedeae870d72a168ef42c98e31f291e5afc6729b
Sara Nolen <nolensara1@gmail.com>
2021-02-11 21:33:52 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,10 +24,10 @@ class Transfer
   end
 
   def reverse_transfer
-    if valid? && receiver.balance > amount && self.status =="pending"
+    if valid? && receiver.balance > amount && self.status =="complete"
       receiver.withdrawl(self.amount)
       sender.deposit(self.amount)
-      self.status = "complete"
+      self.status = "reversed"
     else
       self.status = "rejected"
       "Transaction rejected. Please check your account balance."
